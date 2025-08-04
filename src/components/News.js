f460e250032db7d9c2a73605b71b20856f2473c7
sana farooq <sf4563496@gmail.com>
2025-08-04 06:16:04 -0700

    Adding Previous and Next Button

--- src/components/News.js
+++ src/components/News.js
@@ -5,16 +5,45 @@ export class News extends Component {
     constructor(){
         super();
         this.state ={ 
+            page: 1,
            articles: [],
            loading: false
         }
     }
 
     async componentDidMount(){
-      let  url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73";
+      let  url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page1&pageSize=20";
       let data = await fetch(url);
       let parsedata=await data.json();
-      this.setState({articles: parsedata.articles});
+      this.setState({articles: parsedata.articles , totalResults: parsedata.totalResults});
+    }
+
+    HnadlePreviousClick= async() =>{
+       let  url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=${this.state.page-1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedata= await data.json();
+
+      this.setState({
+        page: this.state.page-1,
+        articles: parsedata.articles,
+      })
+        }
+
+    HnadleNextClick= async() =>{
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+        
+        }
+        else{
+      let  url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=${this.state.page+1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedata=await data.json();
+      
+
+      this.setState({
+        page: this.state.page+1,
+        articles: parsedata.articles,
+      })
+      }
     }
   render() {
 
@@ -28,6 +57,10 @@ export class News extends Component {
                 </div>
                 })}
                 
+            </div>
+            <div className="container d-flex justify-content-between py-3">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.HnadlePreviousClick}>&laquo; Previous</button>
+                <button type="button" className="btn btn-dark" onClick={this.HnadleNextClick}>Next &raquo;</button>
             </div>
         </div>
       </div>
