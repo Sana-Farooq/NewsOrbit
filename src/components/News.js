fc3d94d539ea099b4e143448b494467cc0b95d87
sana farooq <sf4563496@gmail.com>
2025-08-05 23:11:16 -0700

    improve NavBar UI with dynamic categories and active link styling

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
 
 export class News extends Component {
+  static defaultProps={
+     country: 'us',
+     pageSize: 8,
+     category: 'general',
+
+  }
+  static PropsTypes={
+     country: PropTypes.string,
+     pageSize: PropTypes.number,
+  }
     constructor(){
         super();
         this.state ={ 
@@ -14,7 +25,7 @@ export class News extends Component {
     }
 
     async componentDidMount(){
-      let  url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=1&pageSize=${this.props.pageSize}`;
+      let  url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=1&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedata=await data.json();
@@ -24,7 +35,7 @@ export class News extends Component {
     }
 
     HandlePreviousClick= async() =>{
-      let  url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+      let  url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedata= await data.json();
@@ -38,7 +49,7 @@ export class News extends Component {
     HandleNextClick= async() =>{
       if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
       }else{
-      let  url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      let  url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=024672c0c7ec4079b88cb23f8e8d9c73&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedata=await data.json();
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
       let {title, description, imageUrl, NewsUrl}= this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={imageUrl?imageUrl:"https://techcrunch.com/wp-content/uploads/2024/04/GettyImages-1708316139.jpg?resize=1200,800"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
